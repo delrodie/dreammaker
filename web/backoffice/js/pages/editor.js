c2aff6f02278f53475c9d30da76593a089ee29a2
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-02-07 13:19:49 +0000

    Backend gestion des services

--- web/backoffice/js/pages/editor.js
+++ web/backoffice/js/pages/editor.js
@@ -8,7 +8,8 @@
     $(function () {
 	//bootstrap WYSIHTML5 - text editor
     $('#appbundle_presentation_contenu').wysihtml5();
-    $('.contenu').wysihtml5();		
+    $('#appbundle_service_contenu').wysihtml5();
+    $('.contenu').wysihtml5();
 	
   });
 
