c90326ece862710394eec5bb6ae09cf016ca19ce
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2019-06-25 17:29:04 +0000

    Frontend Concours et reglement

--- src/AppBundle/Controller/Frontend/ConcoursController.php
+++ src/AppBundle/Controller/Frontend/ConcoursController.php
@@ -30,9 +30,38 @@ class ConcoursController extends Controller
      */
     public function showAction(Concours $concours)
     {
-        return $this->render("frontend/concours_show.html.html.twig",[
+        return $this->render("concours_show.html.twig",[
             'concour' => $concours,
             'current_menu' => 'actualite'
+        ]);
+    }
+
+    /**
+     * @Route("/{concours}/reglement", name="frontend_concours_reglement")
+     * @Method("GET")
+     */
+    public function reglementAction($concours)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $concours = $em->getRepository("AppBundle:Concours")->findOneBy(['slug'=>$concours]);
+        $reglement = $em->getRepository("AppBundle:Reglement")->findOneBy(['concours'=>$concours]);
+        return $this->render("frontend/reglement_show.html.twig",[
+            'reglement' => $reglement,
+            'current_menu' => 'event'
+        ]);
+    }
+
+    /**
+     * @Route("/item/{reglement}", name="frontend_reglement_item")
+     * @Method("GET")
+     */
+    public function itemAction($reglement)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $reglement = $em->getRepository("AppBundle:Reglement")->findOneBy(['id'=>$reglement]);
+        $items = $em->getRepository("AppBundle:Item")->findBy(['reglement'=>$reglement]);
+        return $this->render("frontend/item.html.twig",[
+            'items' => $items
         ]);
     }
 }
